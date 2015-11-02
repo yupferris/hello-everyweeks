be1e034d7488aaf6dc413ff8a1bbaa95ecb02369
yupferris <jake@fusetools.com>
2015-11-01 21:08:32 -0800

    sanity commit :)

--- hello.v
+++ hello.v
@@ -1,7 +1,55 @@
-module hello(input clk_50, input reset_n, output tx);
+`define STATE_IDLE                   3'h0
+`define STATE_BYTE_START             3'h1
+`define STATE_BYTE_WAIT_FOR_BUSY     3'h2
+`define STATE_BYTE_WAIT_FOR_NOT_BUSY 3'h3
+`define STATE_STOPPED                3'h4
+
+module hello(
+             input  clk_50,
+             input  reset_n,
+             output tx);
+
+   reg [7:0] data;
+   reg       dataReady;
+
+   wire      busy;
+
+   reg [7:0] message;
+   reg [2:0] state;
+
+   always @(posedge clk_50 or negedge reset_n)
+     if (reset_n == 1'b0) begin
+        data <= 0;
+        dataReady <= 0;
+
+        message <= "Q";
+        state <= `STATE_IDLE;
+     end else begin
+        case (state)
+          `STATE_IDLE: state <= `STATE_BYTE_START;
+          `STATE_BYTE_START: begin
+             data <= message[7:0];
+             dataReady <= 1;
+             state <= `STATE_BYTE_WAIT_FOR_BUSY;
+          end
+          `STATE_BYTE_WAIT_FOR_BUSY:
+            if (busy) begin
+               dataReady <= 0;
+               state <= `STATE_BYTE_WAIT_FOR_NOT_BUSY;
+            end
+          `STATE_BYTE_WAIT_FOR_NOT_BUSY:
+            if (!busy)
+              state <= `STATE_STOPPED;//`STATE_IDLE;
+          default: state <= `STATE_STOPPED;
+        endcase
+     end
+
    uart yuart(
-         .clk(clk_50),
-         .reset(reset_n),
-         .tx(tx)
-         );
+              .clk(clk_50),
+              .reset(!reset_n),
+              .data(data),
+              .dataReady(dataReady),
+              .busy(busy),
+              .tx(tx)
+              );
 endmodule
--- uart.v
+++ uart.v
@@ -11,7 +11,14 @@
 `define STATE_STOP_BIT  4'ha
 `define STATE_STOPPED   4'hb
 
-module uart(input clk, input reset, output reg tx);
+module uart(
+            input       clk,
+            input       reset,
+            input [7:0] data,
+            input       dataReady,
+            output reg  busy,
+            output reg  tx);
+
    parameter CLK_FREQ = 50000000;
    parameter BAUD = 9600;
    parameter BAUD_GEN_ACC_WIDTH = 16;
@@ -20,21 +27,24 @@ module uart(input clk, input reset, output reg tx);
    reg [BAUD_GEN_ACC_WIDTH:0] baudGenAcc;
    always @(posedge clk or posedge reset)
      if (reset)
-        baudGenAcc <= 0;
+       baudGenAcc <= 0;
      else
-        baudGenAcc <= baudGenAcc[BAUD_GEN_ACC_WIDTH - 1 : 0] + BAUD_GEN_INC;
+       baudGenAcc <= baudGenAcc[BAUD_GEN_ACC_WIDTH - 1 : 0] + BAUD_GEN_INC;
    wire baudClk = baudGenAcc[BAUD_GEN_ACC_WIDTH];
 
-   reg [7:0] data;
    reg [3:0] state;
+   reg [7:0] dataBuf;
 
    always @(posedge clk or posedge reset)
      if (reset) begin
-        data <= "a";
         state <= `STATE_IDLE;
      end else
        case (state)
-         `STATE_IDLE: state <= `STATE_START_BIT;
+         `STATE_IDLE:
+           if (dataReady) begin
+              state <= `STATE_START_BIT;
+              dataBuf <= data;
+           end
          `STATE_START_BIT: if (baudClk) state <= `STATE_BIT_0;
          `STATE_BIT_0: if (baudClk) state <= `STATE_BIT_1;
          `STATE_BIT_1: if (baudClk) state <= `STATE_BIT_2;
@@ -44,20 +54,28 @@ module uart(input clk, input reset, output reg tx);
          `STATE_BIT_5: if (baudClk) state <= `STATE_BIT_6;
          `STATE_BIT_6: if (baudClk) state <= `STATE_BIT_7;
          `STATE_BIT_7: if (baudClk) state <= `STATE_STOP_BIT;
+         `STATE_STOP_BIT: if (baudClk) state <= `STATE_IDLE;
          default: if(baudClk) state <= `STATE_STOPPED;
        endcase
 
-   always @(state)
+   always @(state or dataBuf)
      case (state)
        `STATE_START_BIT: tx <= 0;
-       `STATE_BIT_0: tx <= data[0];
-       `STATE_BIT_1: tx <= data[1];
-       `STATE_BIT_2: tx <= data[2];
-       `STATE_BIT_3: tx <= data[3];
-       `STATE_BIT_4: tx <= data[4];
-       `STATE_BIT_5: tx <= data[5];
-       `STATE_BIT_6: tx <= data[6];
-       `STATE_BIT_7: tx <= data[7];
+       `STATE_BIT_0: tx <= dataBuf[0];
+       `STATE_BIT_1: tx <= dataBuf[1];
+       `STATE_BIT_2: tx <= dataBuf[2];
+       `STATE_BIT_3: tx <= dataBuf[3];
+       `STATE_BIT_4: tx <= dataBuf[4];
+       `STATE_BIT_5: tx <= dataBuf[5];
+       `STATE_BIT_6: tx <= dataBuf[6];
+       `STATE_BIT_7: tx <= dataBuf[7];
        default: tx <= 1;
+     endcase
+
+   always @(state)
+     case (state)
+       `STATE_IDLE: busy <= 0;
+       `STATE_STOPPED: busy <= 0;
+       default: busy <= 1;
      endcase
 endmodule
