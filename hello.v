cf0a8786572968977da2bcfad8010bc190b32a18
yupferris <jake@fusetools.com>
2015-11-01 21:27:33 -0800

    guys it works omg GUYS

--- hello.v
+++ hello.v
@@ -3,6 +3,10 @@
 `define STATE_BYTE_WAIT_FOR_NOT_BUSY 3'h2
 `define STATE_STOPPED                3'h3
 
+`define MESSAGE "Hello, everyweeks!\r\n"
+`define MESSAGE_LEN 20
+`define MESSAGE_LEN_BITS `MESSAGE_LEN * 8
+
 module hello(
              input  clk_50,
              input  reset_n,
@@ -13,7 +17,7 @@ module hello(
 
    wire      busy;
 
-   reg [0:15] message;
+   reg [0:`MESSAGE_LEN_BITS - 1] message;
 
    reg [1:0] state;
 
@@ -22,14 +26,14 @@ module hello(
         data <= 0;
         dataReady <= 0;
 
-        message <= ":)";
+        message <= `MESSAGE;
         state <= `STATE_BYTE_START;
      end else begin
         case (state)
           `STATE_BYTE_START:
             if (message[0:7] != 0) begin
                data <= message[0:7];
-               message <= {message[8:15], 8'd0};
+               message <= {message[8:`MESSAGE_LEN_BITS - 1], 8'd0};
                dataReady <= 1;
                state <= `STATE_BYTE_WAIT_FOR_BUSY;
             end else
